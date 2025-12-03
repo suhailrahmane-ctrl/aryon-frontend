3fa9fd70e57a0fc1be8d95ca7b44ebfc7b0ccd34
suhailrahmane-ctrl <suhailrahmane@gmail.com>
2025-12-03 22:24:43 +0430

    Create ImagePage component for image generation

---
+++ app/image/page.js
@@ -0,0 +1,8 @@
+export default function ImagePage() {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Generate Images</h1>
+      <p className="text-gray-600">This is where image generation will go.</p>
+    </div>
+  );
+}
