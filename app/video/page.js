8f3c6d07fe440e8d3e97d5069d0ced421659f0d1
suhailrahmane-ctrl <suhailrahmane@gmail.com>
2025-12-03 22:25:55 +0430

    Create page.js

---
+++ app/video/page.js
@@ -0,0 +1,8 @@
+export default function VideoPage() {
+  return (
+    <div className="max-w-3xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4">Generate Videos</h1>
+      <p className="text-gray-600">This is where video generation will go.</p>
+    </div>
+  );
+}
