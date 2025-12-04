663bbf27bf9c4f559df1f52f210a4178d2e0b1eb
suhailrahmane-ctrl <suhailrahmane@gmail.com>
2025-12-04 08:16:22 +0430

    Delete app/video directory

--- app/video/paga.js
+++
@@ -1,3 +0,0 @@
-export default function VideoPage() {
-  return <h1>Video Page</h1>;
-}
--- app/video/page.js
+++
@@ -1,8 +0,0 @@
-export default function VideoPage() {
-  return (
-    <div className="max-w-3xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Generate Videos</h1>
-      <p className="text-gray-600">This is where video generation will go.</p>
-    </div>
-  );
-}
